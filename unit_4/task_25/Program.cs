61482d14a15956df607046eb31a792a723a75641
Marat <ldsredguard@yandex.ru>
2022-12-10 23:29:07 +0700

    Fix format

--- unit_4/task_25/Program.cs
+++ unit_4/task_25/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-int NumPower (int a, int b)
+int NumPower(int a, int b)
 {
     int res = 1;
     for (int i = 1; i <= b; i++)
     {
-        res = res*a;
+        res = res * a;
     }
     return res;
 }
--- unit_4/task_27/Program.cs
+++ unit_4/task_27/Program.cs
@@ -2,13 +2,13 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
-int FindSum (int num)
+int FindSum(int num)
 {
     int sum = 0;
-    while (num>0)
+    while (num > 0)
     {
-        sum = sum + num%10;
-        num = num/10;
+        sum = sum + num % 10;
+        num = num / 10;
     }
     return sum;
 }
