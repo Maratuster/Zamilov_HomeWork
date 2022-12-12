2b4ab71ec81558e938bb2c7232b073e3e022c390
Maratuster <92256006+Maratuster@users.noreply.github.com>
2022-12-12 13:52:50 +0700

    Some work at task 38

--- unit_5/task_38/Program.cs
+++ unit_5/task_38/Program.cs
@@ -2,22 +2,37 @@
 Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 [3 7 22 2 78] -> 76
 */
-// Функция заполнения массива случайными значениями в пределах заданных
+// Функция заполнения массива случайными вещественными значениями в пределах заданных
 double[] GetArray (int size, int minV, int maxV)
 {
     double[] tempArray = new double[size];
     for (int i = 0; i < size; i++)
     {
-        tempArray[i] = Math.Round(new Random().Next(minV, maxV+1), 2);
+        tempArray[i] = new Random().Next(minV, maxV+1);
+        //tempArray[i] = Math.Round(tempArray[i], 2);
     }
     Console.Write(string.Join(", ", tempArray));
     return tempArray;
 }
 
+// Функция нахождения парных сумм начальных и конечных значений массива
+double GetSum (double[] tempArray, int size)
+{
+    double[] newArray = new double[(size/2)+1];
+    for (int i = 0; i < (size/2); i++)
+    {
+        newArray[i] = tempArray[i] + tempArray[size-1-i];
+        Console.Write($"{newArray}, ");
+    }
+    Console.Write(string.Join(", ", newArray));
+    return newArray;
+}
+
 Console.Write("Задайте длину массива: ");
-int arraySize = Convert.ToDouble(Console.ReadLine());
+int arraySize = Convert.ToInt32(Console.ReadLine());
 Console.Write("Задайте левую границу массива: ");
-int leftValue = Convert.ToDouble(Console.ReadLine());
+int leftValue = Convert.ToInt32(Console.ReadLine());
 Console.Write("Задайте правую границу массива: ");
-int rightValue = Convert.ToDouble(Console.ReadLine());
+int rightValue = Convert.ToInt32(Console.ReadLine());
 double[] array = GetArray(arraySize, leftValue, rightValue);
+array = GetSum(array, rightValue);
