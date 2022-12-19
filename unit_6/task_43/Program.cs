94dde83239cd3d123b40017f37425e82439bd7e8
Marat <ldsredguard@yandex.ru>
2022-12-19 22:12:49 +0700

    Add task_43

--- unit_6/task_43/Program.cs
+++ unit_6/task_43/Program.cs
@@ -4,8 +4,25 @@
 3 -> 11
 2 -> 10
 */
+void GetBinariNumber (int num)
+{
+string twoNum = "";
+while (num != 0)
+{
+    twoNum = twoNum + (char)num%2;
+    num/=2;
+}
+    char[] twoNumA = new char[twoNum.Length];
+    for (int i = 0; i < twoNumA.Length; i++)
+    {
+        twoNumA[i] = twoNum[^(i+1)];
+    }
+    Console.WriteLine(string.Join("", twoNumA));
+}
+
+
     
-    
-    
-    Console.Write("Введите число k1: ");
-    int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+GetBinariNumber (number);
+
