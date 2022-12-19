8de390e6d719b380b6c60027cdfddc1a302eb779
Maratuster <92256006+Maratuster@users.noreply.github.com>
2022-12-19 19:44:10 +0700

    Add task_41 & task_42

---
+++ unit_6/task_41/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+int[] GetArray (int size)
+{
+    int[] tempArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите число и нажмите интер: ");
+        tempArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return tempArray;
+}
+
+// Функция нахождения количества чётных значений среди элементов массива
+int EvenCount (int[] collection)
+{
+    int count = 0;
+    foreach (var i in collection)
+    {
+        if (i>0) count++;
+    }
+    return count;
+}
+
+Console.Write("Задайте длину массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] array = GetArray(arraySize);
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine(EvenCount(array));
