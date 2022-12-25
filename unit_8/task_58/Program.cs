9eb0044b8df845583e41cfc6a072e9cded19156a
Marat <ldsredguard@yandex.ru>
2022-12-26 00:03:32 +0700

    Add task_58

---
+++ unit_8/task_58/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+int[,] GetMatrix(int m, int n, int minValue, int maxValue)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+        {
+            Console.Write(inputMatrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ArrayElementsProduct(int[,] firstMatrix, int[,] secondMatrix)
+{
+    int[,] matrix = new int[firstMatrix.GetLength(0), firstMatrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = firstMatrix[i,j]*secondMatrix[i,j];
+        }
+    }
+    return matrix;
+}
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] firstArray = GetMatrix(rows, columns, 0, 10);
+PrintMatrix(firstArray);
+Console.WriteLine();
+int[,] secondArray = GetMatrix(rows, columns, 0, 10);
+PrintMatrix(secondArray);
+Console.WriteLine();
+PrintMatrix(ArrayElementsProduct(firstArray, secondArray));
