8b4790098edc0bf392f161e598536360b3473afe
Maratuster <92256006+Maratuster@users.noreply.github.com>
2022-12-26 13:03:45 +0700

    Add task_62

--- unit_8/task_62/Program.cs
+++ unit_8/task_62/Program.cs
@@ -6,30 +6,81 @@
 11 16 15 06
 10 09 08 07
 */
-int[,] GetSpiralMatrix(int n)
+/// Данная функция будет работать при условии, что количество строк меньше количества столбцов
+int[,] GetSpiralMatrix(int row, int column)
 {
-    int[,] matrix = new int[n, n];
-    // Ниже спирачено с интернета. Буду разбираться в коде. Пока не понятно ничего
-    int elementMean = 1;
-    int count = n;
-    int value = -n;
-    int position = -1;
-    do {
-        value = -1 * value / n;
-        for (int i = 0; i < count; i++) {
+    int[,] matrix = new int[row, column];
+    int elementMean = 1; // Заводим переменную, значение которой буде присваивать элементам массива
+    int countRow = row; // Заводим счётчик строк для прохода по столбцам.
+    int countColumn = column; // Заводим счётчик столбцов для прохода по строкам.
+    int value = -column; // задаём переменную, которая выступит чем-то типа коэффициента для получения индекса элемента
+    int position = -1; // задаём переменную, которая выступит одним из показателе для получения индекса элемента
+    //Далее мы создаём цикл, который за проход заполняет массив по горизонтали и по вертикали. Количество проходов напрямую зависит от количества строк.
+    while (countRow > 0)
+    {
+        value = -1 * value / column; // при проходе по строке коэффициент зависит от количества строк
+        for (int i = 0; i < countColumn; i++)
+        {
             position += value;
-            matrix[position / n, position % n] = elementMean++;
+            matrix[position / column, position % column] = elementMean;
+            elementMean++;
         }
-        value *= n;
-        count--;
-        for (int i = 0; i < count; i++) {
-            position += value;  
-            matrix[position / n, position % n] = elementMean++;
+        value *= column; // при проходе по столбцам коэффициент зависит от количества столбцов
+        countRow--;
+        for (int i = 0; i < countRow; i++)
+        {
+            position += value;
+            matrix[position / column, position % column] = elementMean;
+            elementMean++;
         }
-    } while (count > 0);
+        countColumn--;
+    }
 
     return matrix;
 }
+
+/// Данная функция будет работать при условии, что количество столбцов меньше, чем строк количество строк
+/// Для работы данной функции мы копируем содержание GetSpiralMatrix(), заменяя соответственно значения коэффициента value.
+/// Заполнение первой строки выносим в отдельный цикл за пределами While. Это даёт нам направление заполнения массива по часовой стрелке.
+int[,] GetSpiralMatrixClone(int row, int column)
+{
+    int[,] matrix = new int[row, column];
+    int elementMean = 1;
+    int countRow = row;
+    int countColumn = column;
+    int value = -row;
+    int position = -1;
+    value = -1 * value / row;
+    for (int i = 0; i < countColumn; i++)
+    {
+        position += value;
+        matrix[position / column, position % column] = elementMean;
+        elementMean++;
+    }
+    countRow--;
+    while (countColumn > 0)
+    {
+
+        value *= column;
+        for (int i = 0; i < countRow; i++)
+        {
+            position += value;
+            matrix[position / column, position % column] = elementMean;
+            elementMean++;
+        }
+        countColumn--;
+        value = -1 * value / column;
+        for (int i = 0; i < countColumn; i++)
+        {
+            position += value;
+            matrix[position / column, position % column] = elementMean;
+            elementMean++;
+        }
+        countRow--;
+    }
+    return matrix;
+}
+
 void PrintMatrix(int[,] inputMatrix)
 {
     for (int i = 0; i < inputMatrix.GetLength(0); i++)
@@ -44,7 +95,9 @@ void PrintMatrix(int[,] inputMatrix)
 
 Console.Write("Введите количество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите количество столбцов: ");
-// int columns = Convert.ToInt32(Console.ReadLine());
-int[,] result = GetSpiralMatrix(rows);
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] result = new int[rows, columns];
+if (columns >= rows) result = GetSpiralMatrix(rows, columns);
+else result = GetSpiralMatrixClone(rows, columns);
 PrintMatrix(result);
